d8076b048ad8a201590bd558b5c887abef4b596f
ReshefK <reshef.ka@gmail.com>
2023-03-30 09:29:23 +0300

    update 30/3

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -2,6 +2,8 @@ import "./Project.css";
 import lily from "../../Assets/lily.jpg"
 import ballon from "../../Assets/ballon.jpg"
 import canyon1 from "../../Assets/canyon1.jpg"
+import sale from "../../Assets/sale.jpg"
+import todo from "../../Assets/todoNote.jpg"
 
 function Project(): JSX.Element {
     return (
@@ -14,7 +16,7 @@ function Project(): JSX.Element {
             <div className="container">
                                 <div className="project__img__box">
                                     <div className="card">
-                                    <img className="img" src={lily} alt="shopping"></img>
+                                    <img className="img" src={sale} alt="shopping"></img>
                                         <div className="fon"></div>
                                             <div className="ProjectText">
                                                 <h2>
@@ -34,19 +36,22 @@ function Project(): JSX.Element {
 
                                 
                                     <div className="card">
-                                    <img className="img" src={ballon} alt="ballon"></img>
+                                    <img className="img" src={todo} alt="ballon"></img>
                                         <div className="fon"></div>
                                             <div className="ProjectText">
                                                 <h2>
-                                                    Transition
+                                                    Todo list project
                                                 </h2>
                                                     <p>
-                                                    More project coming soon!
+                                                    Todo app project, create edit and delete tasks.  
+                                                    Also add the ability to translate task using microsoft translate API.
                                                     </p>
                                                 <div className="triangle"></div>
                                             </div>
-                                            <div className="btn">
-                                            View
+                                            <div>
+                                            <a href="https://github.com/ReshefK/todo-app.git" target="_blank" rel="noopener noreferrer" className="btn">
+                                            View Code
+                                            </a>
                                             </div>
                                     </div>
 
